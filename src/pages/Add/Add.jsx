bb0961fc868b2cf23bb62bcd7c10067ef3f45813
NeringaSun <82957315+NeringaSun@users.noreply.github.com>
2021-12-14 17:54:05 +0200

    added home data

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,18 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Header, Navigation } from '../../components';
 
 const pages = [
-  { url: '/Home', name: 'Home' },
+  { url: '/', name: 'Home' },
   { url: '/Add', name: 'Add' },
 ];
 
 const Add = () => {
+  const token = window.localStorage.getItem('token');
+  const [userInputs, setUserInputs] = useState([]);
+  const Navigate = useNavigate();
+
   return (
     <div>
       <Header>
         <Navigation links={pages} />
       </Header>
-      Add
+      <div>
+        <div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              fetch(`${process.env.REACT_APP_BASE_URL}/v1/content/skills`, {
+                method: 'POST',
+                headers: {
+                  authorization: `Bearer ${token}`,
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userInputs),
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  if (data) {
+                    alert('Skills added succesfully!');
+                    setUserInputs('');
+                    Navigate('/', { replace: true });
+                  }
+                })
+                .catch((err) => alert(err.message))
+                .finally(() => e.target.reset());
+            }}
+          >
+            <h1>ADD SKILLS</h1>
+            <div>
+              <div>
+                <label className='label'>Title</label>
+                <input
+                  className='input'
+                  type='text'
+                  placeholder='Title'
+                  onChange={(e) =>
+                    setUserInputs({
+                      ...userInputs,
+                      title: e.target.value.trim(),
+                    })
+                  }
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <label className='label'>Description</label>
+                <textarea
+                  className='textarea'
+                  onChange={(e) =>
+                    setUserInputs({
+                      ...userInputs,
+                      description: e.target.value.trim(),
+                    })
+                  }
+                  placeholder='Description'
+                  required
+                ></textarea>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <button>ADD SKILLS</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
-import { Header, Navigation } from '../../components';
+import React, { useEffect, useState } from 'react';
+import { Header, Container, Navigation } from '../../components';
 
 const pages = [
-  { url: '/Home', name: 'Home' },
+  { url: '/', name: 'Home' },
   { url: '/Add', name: 'Add' },
 ];
 
 const Home = () => {
+  const token = window.localStorage.getItem('token');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BASE_URL}/v1/content/skills`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data || token) {
+          setData(data);
+          console.log(data);
+        } else if (!data.content) {
+          alert('No data found');
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [token]);
+
   return (
     <div>
       <Header>
         <Navigation links={pages} />
       </Header>
-      Home
+      <Container>
+        <div>
+          {!data && <h1 className='title'>Loading...</h1>}
+          <div>
+            {data.map((item, i) => (
+              <div id={i}>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
